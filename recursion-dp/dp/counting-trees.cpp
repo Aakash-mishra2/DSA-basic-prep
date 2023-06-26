f61dc7d6d821fffcd521a844d36c2d18c92679ad
Aakash Mishra <aakashmishra.281@gmail.com>
2023-06-26 15:23:51 +0530

    26 june new-heirarchyTree

--- recursion-dp/dp/counting-trees.cpp
+++ recursion-dp/dp/counting-trees.cpp
@@ -1,10 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+int counting_trees(int nodes)
+{
+    vector<int> dp(nodes + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= nodes; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            dp[i] += (dp[j - 1] * dp[i - j]);
+        }
+    }
+    return dp[nodes];
+}
 
 int main()
 {
 
-    // box - array
+    int n;
+    cin >> n;
+
+    cout << counting_trees(n) << endl;
 
     return 0;
 }

c2d724925e0c66e30edfdba3bf9e46c6405f98d3
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-08-16 09:07:14 +0530

    Update counting-BS-trees.cpp

--- trees/counting-BS-trees.cpp
+++ trees/counting-BS-trees.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countingTrees(int nodes)
-{
-}
 int countTreesDP(int nodes)
 {
+    vector<int> dp(nodes + 1, 0);
+    dp[0] = dp[1] = 1;
+    for (int i = 2; i <= nodes; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    return dp[nodes];
 }
 int main()
 {

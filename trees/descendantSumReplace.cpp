ddfa4064ca95c7a57d4675c5172773a5b43380f2
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-08-16 09:42:27 +0530

    Update descendantSumReplace.cpp

--- trees/descendantSumReplace.cpp
+++ trees/descendantSumReplace.cpp
@@ -1,25 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class node
+void file_io()
 {
-};
-
-node *buildTree()
-{
+    ios_base::sync_with_stdio;
+    cout.tie(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("treeinput.txt", "r", stdin);
+    freopen("treeoutput.txt", "w", stdout);
+#endif
 }
 
+class node
+{
+public:
+    int data;
+    node *left = nullptr;
+    node *right = nullptr;
+    node(int val) : data(val) {}
+};
+
+node *preOrderbuildTree()
+{
+    int d;
+    cin >> d;
+    if (d == -1)
+    {
+        return nullptr;
+    }
+    node *temp = new node(d);
+    temp->left = preOrderbuildTree();
+    temp->right = preOrderbuildTree();
+    return temp;
+}
 void levelOrderPrint(node *root)
 {
+    if (root == nullptr)
+    {
+        return;
+    }
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        if (temp == NULL)
+        {
+            cout << endl;
+            q.pop();
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            q.pop();
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+    return;
 }
 // Todo: Replace with descendant Sum leaving leaf nodes intact
 int replaceWithSum(node *root)
 {
+    if (root == nullptr)
+    {
+        return 0;
+    }
+    if (root->left == nullptr and root->right == nullptr)
+    {
+        return root->data;
+    }
+    int temp = root->data;
+    int SL = replaceWithSum(root->left);
+    int SR = replaceWithSum(root->right);
+    root->data = SL + SR;
+    return temp + root->data;
 }
 int main()
 {
-    freopen("treeinput.txt", "r", stdin);
-    node *root = buildTree();
+    file_io();
+    node *root = preOrderbuildTree();
     levelOrderPrint(root);
     cout << endl;
     replaceWithSum(root);

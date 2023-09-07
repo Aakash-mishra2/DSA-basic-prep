3fb80bf983e5b9e84c3bbf2f7681f31bc4092d0c
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-07 21:18:11 +0530

    Update closestInBST.cpp

--- BST/closestInBST.cpp
+++ BST/closestInBST.cpp
@@ -1,32 +1,68 @@
-// iterative app has better space com and O(1) comp.
+#include<bits/stdc++.h>
+using namespace std;
 
-int closestInBST(int target, Node *root, int &min = INT_MAX)
+void file_io(){
+	ios_base::sync_with_stdio;
+	cout.tie(0); cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+class node{
+public:
+	int data;
+	node* right = nullptr;
+	node* left = nullptr;
+	node(int data): data(data){}
+};
+node* addNode(node* root, int val){
+	if(root == nullptr){ return new node(val);}
+	else if(root->data < val){
+			root->right = addNode(root->right, val);
+		}
+	else
+			root->left = addNode(root->left, val);
+	return root;
+}
+void printBST(node *root)
 {
-
-    int closest;
-    int diff = INT_MAX;
-    Node *temp = root;
-    while (temp != NULL)
+    if (root == nullptr)
     {
-        int current_diff = abs(temp->key - target);
-        if (current_diff == 0)
-        {
-            return temp->key;
-        }
-
-        if (current_diff < diff)
-        {
-            diff = current_diff;
-            closest = root->key;
-        }
-        if (temp->key < target)
-        {
-            temp = temp->right;
-        }
-        else
-        {
-            temp = temp->left;
-        }
+        return;
     }
-    return closest;
+    printBST(root->left);
+    cout << root->data << " ";
+    printBST(root->right);
+}
+int closest(int target, node*root){
+	int closest, diff=INT_MAX;
+	node* temp = root;
+	while( temp !=  nullptr){
+		int current = abs(temp->data - target);
+		if( current == 0) return temp->data;
+		else if( current < diff){
+			closest = temp->data;
+			diff = current;
+		}
+		if(temp->data < target){
+			temp = temp->right;
+		}
+		else temp = temp->left;
+	}
+	return closest;
+}
+int main(){
+	file_io();
+	node* root = nullptr;
+	vector<int> arr = { 12, 6, 1, 14, 7, 3, 11};
+	for (int i = 0; i < arr.size(); i++)
+    {
+        root = addNode(root, arr[i]);
+    }
+     printBST(root);
+	 cout << endl;
+	 int key = 3;
+    cout<<" Node closest to "<<key<<" is : "<<closest(key, root);
+	return 0;
 }

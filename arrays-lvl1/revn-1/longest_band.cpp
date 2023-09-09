0a6becceaea311fd09c318602232ed2098954b7d
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-09 08:46:27 +0530

    Update longest_band.cpp

--- arrays-lvl1/revn-1/longest_band.cpp
+++ arrays-lvl1/revn-1/longest_band.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string longest_band(vector<int> arr, int i = 0)
+int longest_band(vector<int> arr, int i = 0)
 {
+    unordered_set<int> s;
+    for( auto i : arr ){ s.insert(i); }
+    int answer = 0;
+    for( auto ele : s){
+        int band_len = 1;
+        if( s.find(ele-1) != s.end()){ continue; }
+        else{
+            while(s.find(ele+1)!=s.end()) {
+                band_len += 1;
+                ele += 1;
+            }
+            if( band_len > answer) answer = band_len;
+        }
+    }
+    return answer;
 }
 int main()
 {

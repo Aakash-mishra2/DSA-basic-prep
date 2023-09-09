ae8e99e96ef6eb5d69f4381df1eadbbfc460de9d
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-09 21:16:58 +0530

    Update readingBooks.cpp

--- bsearch/readingBooks.cpp
+++ bsearch/readingBooks.cpp
@@ -1,52 +1,47 @@
+//Given number of pages in n different books and m students. The are arranged in any order(not necessarily ascendign) of number of pages. 
+//Every student is assigned to read some consecutive segment of books. Thes task is to assign books in such a way that the maximum of number  of 
+//pages asigned to a student is minimum.
+//Find the maxinum number of pages that a student will have to read.
 #include<bits/stdc++.h>
 using namespace std;
 
-int canAssign(vector<int> books, int pages){
-    int stud = 1, pagesAssigned = 0;
-    for( int i = 0; i<books.size(); i++){
-        if(pagesAssigned + books[i] <= pages){
-            pagesAssigned += books[i];
+int_least32_t canAssign(vector<int> books, int pages_limit){
+        int current_pages = 0, assigned = 1;
+        for( int i = 0; i<books.size(); i++){
+            if( current_pages + books[i] <= pages_limit ){
+                current_pages += books[i];
+            }
+            else{
+                assigned += 1;
+                current_pages = books[i];
+            }
+        }
+    return assigned;
+}
+
+int readingBooks(vector<int> books, int students ){
+    int s = 0, e = 0, ans;
+    for(auto i : books){
+        if ( i > s){ s = i; }
+        e += i;
+    }
+
+    while( s<=e ){
+        int mid = s + (e - s)/2;
+        int st = canAssign(books, mid);
+        if( st <= students ){
+            ans = mid;
+            e = mid - 1;
         }
         else{
-            stud++;
-            pagesAssigned = books[i];
+            s = mid + 1;
         }
     }
-    return stud;
+    return ans;
 }
-
-int readingBooks(vector<int> books, int m){
-  int s = 0, e = 0, ans =0, val = 0;
-  for(auto i : books){
-    s = max(s, i);
-    e = e + i;
-  }
-  while( s <= e){
-    int mid = s + (e-s)/2;
-    int students = canAssign(books, mid);
-    if( students <= m){
-        ans = mid;
-        e = mid - 1;
-    }
-    else s = mid + 1;
-  }
-  return ans;
-}
-//0 1 2 3 -- 2
-//0 1 2 3 4 5 6 7 8 9 10 -- 4
-//0 1 2 3 4 5 6 7 8 9 -- 5
 
 int main(){
-    // vector<int> books = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    // int m = 4;
     vector<int> books = {10, 20, 30, 15};
-    int m = 2;
-
-// student can be assigned either one book or all books
-// max pages possible to assign to a student from one book is book with max pages;
-// max pages possible to assign to a student from all books is sum of all pages;
-// assign books s.t. max no. of pages assigned is minimum.
-
-cout<<readingBooks(books, m);
-
+    int students = 2;
+    cout<<readingBooks(books, students);
 }

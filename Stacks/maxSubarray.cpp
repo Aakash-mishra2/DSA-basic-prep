5982ba8cd60ee3d3a762c58c0b9732d08084d0e9
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-12 05:18:04 +0530

    Update maxSubarray.cpp

--- Stacks/maxSubarray.cpp
+++ Stacks/maxSubarray.cpp
@@ -1,33 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+void file_io()
+{
+    ios_base::sync_with_stdio;
+    cout.tie(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+/*
+N = 9
+arr = {1 2 3 1 4 5 2 3 6}
+k = 3
+3 3 4 5 5 5 6
+*/
 void maxSubArray(vector<int> a, int k){
-    deque<int> Q(k);
-    //process first k elements
-    int i;
-    for(i = 0; i<k; i++ ){
-        while(!Q.empty() && a[i] > a[Q.back()]){
-            Q.pop_back();
+    int i = 0;
+    deque<int> Q;
+    //process first k elements : find highest index 
+
+    for( i = 0; i<k; i++){
+        while(!Q.empty() and a[Q.front()] < a[i]){
+            Q.pop_front();
         }
         Q.push_back(i);
     }
+    //printing highest among first k elements
+    cout<<a[Q.front()]<<" ";
 
     //process rest of elements
-    for( ; i<a.size(); i++){
-
-        //remove elements from left when an index lies outside current window.
+    for(; i<a.size(); i++){
+        //remove elements from left when index lies outside current window
         while(!Q.empty() and Q.front() <= i-k){
             Q.pop_front();
         }
-        //when greater element comes 
-        while(!Q.empty() and a[i]>a[Q.back()]){
+
+        //when greater element comes
+        while(!Q.empty() and a[Q.back()] < a[i]){
             Q.pop_back();
         }
         Q.push_back(i);
+        cout<<a[Q.front()]<<" ";
     }
 }
 int main(){
-    vector<int>arr = {1, 2, 3, 1, 4, 5, 2, 3, 5};
+
+    vector<int>arr = {1, 2, 3, 1, 4, 5, 2, 3, 6};
     int K = 3;
     maxSubArray(arr, K);
     return 0;

9284b2edbfdadaf1b825abe74fac8dae03a4b5a8
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-23 06:00:00 +0530

    Update countingRightTriangles.cpp

--- more_misc/countingRightTriangles.cpp
+++ more_misc/countingRightTriangles.cpp
@@ -1,14 +1,4 @@
-#include<bits/stdc++.h>
-using namespace std;
-void file_io()
-{
-    ios_base::sync_with_stdio;
-    cout.tie(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+
 }
 /*
 Input:
@@ -30,44 +20,53 @@ Output:
 1
 */
 
-class Point
+#include<bits/stdc++.h>
+using namespace std;
+//steps
+//1. Generate all substrings.
+//2. generate their Hash.
+//3. create a hash of this Hash to club all anagrams with same hash value
+//  in single bucket,to get their frequency count.
+//4. From freq count, we can easily count pairs.
+void file_io()
 {
-public:
-    int x, y;
-
-    Point(int x, int y) : x(x), y(y) {}
-};
-int countTriangles(vector<Point> points, int n){
-    unordered_map<int, int> mx;
-    unordered_map<int, int> my;
-    //load all (x, cntX) pairs in #maps.
-    for( int i = 0; i<n; i++){
-        Point p = points[i];
-        mx[p.x]++; my[p.y]++;
-    }
-    int ans= 0;
-    for( int i = 0; i<n; i++){
-        Point p = points[i];
-      // avoid counting current point coordinates it's included
-        ans += (mx[p.x] - 1)*(my[p.y] - 1);
-    }
-    return ans;
+    ios_base::sync_with_stdio;
+    cout.tie(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 }
+
+vector<int> getHashValue( string s, int i, int j){
+    vector<int> vec(26, 0);
+    for(int k = i; k<= j; k++){
+        vec[s[k]-'a'] += 1;
+    }
+    return vec;
+}
+
+int countingAnagrams(string str){
+    map<vector<int>, int> map;
+    for(int i = 0; i<str.length(); i++){
+        for(int j = i; j<str.length(); j++){
+            auto thisHash = getHashValue(str,i,j);
+            map[thisHash]++;
+        }
+    }
+    int answer = 0;
+    for(auto p : map){
+        if(p.second > 1){ answer += (p.second)*(p.second - 1)/2; }
+    }
+    return answer;
+}
+
 int main(){
     file_io();
-    int N;
-    cin >> N;
-    int x, y;
-
-    vector<Point> points;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> x >> y;
-        Point p(x, y);
-        points.push_back(p);
-    }
-
-    cout<<countTriangles(points, N);
-
+    string s;
+    cin>>s;
+    cout<<" Number of anagram pairs are : ";
+    cout<<countingAnagrams(s);
     return 0;
 }

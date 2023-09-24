f2d3f275c6cb94acf30fa6ade11ed260825798f7
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-24 10:20:15 +0530

    Update recBSearch.cpp

--- bsearch/recBSearch.cpp
+++ bsearch/recBSearch.cpp
@@ -1,55 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
-inline namespace array_nature{
-	bool asc = true;
-	bool dsc = true;
-}
-template<typename T>
-ostream& operator<<(ostream& os, vector<T>& star){                 //print Array overload 
-	for(T i : star){ os<<i<<" "; } return os; }
 
-int Bsearch(vector<int>& vec, int& key, int start, int end){    //recursive Binary Search
-	int ans, mid = start + (end - start)/2;
-	if(start > end) return -1;
-	if(vec[mid] == key){ ans = mid; } 
-	else if(asc == true){ 									//if sorted in ascending order
-		if (key > vec[mid]) ans = Bsearch(vec, key, mid+1, end);       //search right half
-		else ans = Bsearch(vec, key, start, mid -1);                    //search left half
-	}
-	else if(dsc == true){								  //if sorted in descending order
-		if (key > vec[mid]) ans = Bsearch(vec, key, start, mid - 1);   //search left half
-		else ans = Bsearch(vec, key, mid + 1, end);                    //search left half
-	}
-	return ans;
+void file_io(){
+    ios_base::sync_with_stdio;
+    cout.tie(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("../MORE_miscc/input.txt", "r", stdin);
+        freopen("../MORE_miscc/output.txt", "w", stdout);
+    #endif
+    }
+inline namespace arrOrder{
+    bool asc = true;
+    bool desc = true;
 }
-bool isSorted_bothWays(vector<int>& arr,int ind){
-	if(ind == 0 ) return asc||dsc ;
 
-	if(asc) asc = asc && (arr[ind] >= arr[ind - 1]);
-	if(dsc) dsc = dsc && (arr[ind] <= arr[ind - 1]);
+bool isSorted(vector<int> arr,int n){
+    if(n == 0){ return asc||desc; }
+    
+    if(asc == true) asc = (asc and arr[n] >= arr[n-1]);
+    if(desc == true) desc = (desc and arr[n] <= arr[n-1]);
 
-	return isSorted_bothWays(arr, ind- 1);
+    return isSorted(arr, n-1);
 }
-void linear_reverse_array(vector<int>& arr){ 		//two pointer linear reverse of array
-	int i = 0, j = arr.size() - 1;
-	while(i<j)	{
-		int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
-		i++; j--; }
+int Bsearch(vector<int> arr,int key, int s, int e){
+    if(s>e){ return -1; }
+    else {
+        int mid = s + (e-s)/2;
+        if(arr[mid] == key){ return mid;}
+        else if(key > arr[mid]){ 
+            if(asc) s = mid + 1;
+            if(desc) e = mid - 1; }
+        else {
+            if(asc) e = mid - 1;
+            if(desc) s = mid + 1;
+        }
+    }
+    return Bsearch(arr, key, s, e);
 }
 int main(){
-	vector<int> arr { -17, 4, 5, 10, 12, 14, 19};						//ascending sorted
-
-	linear_reverse_array(arr);											//to reverse sorted
-
-	// cout<<" Enter key value to search from { "<<arr<<" } : "<<endl;
-	// int key; cin>>key;
-	int key = -17;
-	for(auto i: arr) cout<<i<<" ";
-	if(isSorted_bothWays(arr, arr.size() - 1)){
-		int pos = Bsearch(arr, key, 0, arr.size() - 1);
-		if( pos != -1) cout<<" Key "<<key<<" found at : "<<pos + 1<<" Position."<<endl;
-		else cout<< " key not found . "<<endl;
-	}
-	else { cout<<" Array not sorted. "<<endl; } 
-	return 0;
+    int ans = 0;
+    vector<int> arr { -17, 4, 5, 10, 12, 14, 19};
+    if(isSorted(arr, arr.size()-1) == true){
+        int key; cin>>key;
+        int s = 0; int e = arr.size()-1;
+        ans = Bsearch(arr,key, s, e);
+    }
+    else{ cout<<" Array not sorted! "<<endl; return 0;}
+    if(ans == -1){ cout<<"Key not found!"<<endl;}
+    else cout<<"Key found at position "<<ans + 1<<endl;
+    return 0;
 }

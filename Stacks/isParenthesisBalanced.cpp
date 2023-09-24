72d54f2acb96fd7ad036bec50bfa66fa2cf90221
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-24 13:32:33 +0530

    Update isParenthesisBalanced.cpp

--- Stacks/isParenthesisBalanced.cpp
+++ Stacks/isParenthesisBalanced.cpp
@@ -1,94 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void file_io()
-{
+void file_io(){
     ios_base::sync_with_stdio;
-    cout.tie(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
-bool isParenthesisBalanced(string str)
-{
-    stack<char> s;
-    for (auto ch : str)
-    {
-        switch (ch)
-        {
-        case '(':
-        case '{':
-        case '[':
-        {
-            s.push(ch);
-            break;
-        }
-        case ')':
-        {
-            if (!s.empty() and s.top() == '(')
-            {
-                s.pop();
-            }
-            else
-            {
-                return false;
+    cout.tie(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("../MORE_miscc/input.txt", "r", stdin);
+        freopen("../MORE_miscc/output.txt", "w", stdout);
+    #endif
+    }
+bool isParBal(string str){
+    stack<char>s;
+    for(auto ch : str){
+        switch(ch){
+            case '(':
+            case '{':
+            case '[':
+                        s.push(ch);
+                        break;
+            case '}':{
+                if(!s.empty() and s.top()=='{') {s.pop(); continue;}
+                else return false;
+            }break;
+            case ')': {
+                if(!s.empty() and s.top()=='(') {s.pop(); continue;}
+                else return false;
+            }break;
+            case ']': {
+                if(!s.empty() and s.top()=='[') {s.pop(); continue;}
+                else return false;
             }
             break;
-        }
-
-        case '}':
-        {
-            if (!s.empty() and s.top() == '{')
-            {
-                s.pop();
-            }
-            else
-            {
-                return false;
-            }
-            break;
-        }
-
-        case ']':
-        {
-            if (!s.empty() and s.top() == '[')
-            {
-                s.pop();
-            }
-            else
-            {
-                return false;
-            }
-            break;
-        }
-
-        default:
-            continue;
+            default: continue;
         }
     }
-    if (s.empty() == true)
-    {
-        return true;
-    }
+    if(s.empty()) return true;  
     return false;
 }
-int main()
-{
+int main(){
     file_io();
-    int t;
-    cin >> t;
-    // leave no white spaces bw input strings alphabets
-    while (t--)
-    {
-        string operation;
-        cin >> operation;
-        if (isParenthesisBalanced(operation) == true)
-            cout << "TRUE" << endl;
-        else
-            cout << "FALSE" << endl;
-    }
+    int t; cin>>t;
+    while(t--){
 
-    return 0;
+    string s;
+    cin>>s;
+    if(isParBal(s)) cout<<"Is Balanced"<<endl;
+    else cout<<"Not balanced! "<<endl;
+    }
 }

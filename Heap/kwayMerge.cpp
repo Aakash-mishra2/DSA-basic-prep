133240407ae7aad7d8f63aa98da9d429ab6eaff9
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-26 23:33:30 +0530

    Update kwayMerge.cpp

--- Heap/kwayMerge.cpp
+++ Heap/kwayMerge.cpp
@@ -10,32 +10,32 @@ void file_io()
     freopen("output.txt", "w", stdout);
 #endif
 }
-vector<int> mergeKsorted(vector<vector<int>> &arr)
-{
-    vector<int> answer;
-   priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > s;
-   int k = arr.size();
-   for(int k = 0; k<arr.size(); k++){
-        s.push({arr[k][0], 0, k});
-   }
-    while(!s.empty()){
-        auto temp = s.top();
-        s.pop();
+
+vector<int> mergeKsorted(vector<vector<int>> &arr){
+    int rows = arr.size();
+    vector<int> ans;
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > heap;
+    for(int i = 0; i<rows; i++){    
+        heap.push({arr[i][0], 0, i}); 
+    }
+    while(!heap.empty()){
+        auto temp = heap.top();
+        heap.pop();
         int elem = temp.at(0);
         int ind = temp.at(1);
         int arr_ind = temp.at(2);
 
-        if(ind == arr[arr_ind].size()){ continue; }
+        if(ind == arr[arr_ind].size()){ continue;}
 
-        answer.push_back(elem);
-        s.push({arr[arr_ind][ind+1], ind+1, arr_ind});
+        heap.push({arr[arr_ind][ind+1], ind+1, arr_ind});
+        ans.push_back(elem);
     }
-    return answer;
+    return ans;
 }
 
 int main()
 {
-    file_io();
+   // file_io();
     vector<vector<int>> arr = {{10, 15, 20, 30},
                                {2, 5, 8, 14, 24},
                                {0, 11, 60, 90}};

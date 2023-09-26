dc02faf376bdee9fb5ebfa8ef58e30b95b76a958
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-26 22:43:17 +0530

    Update findNearestCAb.cpp

--- Heap/findNearestCAb.cpp
+++ Heap/findNearestCAb.cpp
@@ -6,6 +6,14 @@ void file_i_o()
     cout.tie(0);
     cin.tie(0);
 #ifndef ONLINE_JUDGE
+    /*
+    5 3
+    C1 1 1
+    C2 2 1
+    C3 3 2
+    C4 0 1
+    C5 2 3
+    */
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
@@ -23,6 +31,9 @@ public:
     }
 };
 
+// bool compare = [](const Car& c1, const Car& c2){
+//     return c1.dist() < c2.dist();
+// };
 class compare
 {
 public:
@@ -34,24 +45,28 @@ public:
 
 void printNearestCar(vector<Car> cars, int k)
 {
-    // create max heap of size K
-
-    priority_queue<Car, vector<Car>, compare> max_heap(cars.begin(), cars.begin() + k);
-    for (int i = k; i < cars.size(); i++)
-    {
+    //priority_queue<Car, vector<Car>, decltype(compare)>heap(cars.begin(), cars.begin()+k);
+    
+    priority_queue<Car, vector<Car>, compare> heap(cars.begin(), cars.begin() + k);
+    
+    //if distance of any cab out of rest cabs is smaller than largest element of heap;
+    //then add it to heap;
+    for(int i = k; i<cars.size(); i++){
         Car car = cars[i];
-        if (car.dist() < max_heap.top().dist())
-        {
-            max_heap.pop();
-            max_heap.push(car);
-        }
+        if(cars[i].dist() < heap.top().dist()){ heap.pop(); heap.push(car); }
     }
-    // all the car inside heap will now be closest k cars
+    //prints K nearest cabs in descending order by distance 
+    // while(!heap.empty()){
+    //     cout<<heap.top().id<<endl;
+    //     heap.pop();
+    // }
+
+    //to print in ascending order by distance;
     vector<Car> output;
-    while (!max_heap.empty())
+    while (!heap.empty())
     {
-        output.push_back(max_heap.top());
-        max_heap.pop();
+        output.push_back(heap.top());
+        heap.pop();
     }
     sort(output.begin(), output.end(), [](const Car A, const Car B)
          { return A.dist() < B.dist(); });

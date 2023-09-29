fcdaf8bb4dfdae2828b5cd95bb271f99f1e08c66
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-09-29 18:57:35 +0530

    Update largestIsland.cpp

--- graphs/largestIsland.cpp
+++ graphs/largestIsland.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dfs(vector<vector<int>> grid, int i, int j, vector<vector<bool>> &visited, int r, int c)
-{
-    int length = 0;
-    if (grid[i][j] == 1)
-        length = 1;
 
-    int dx[] = {+1, -1, 0, 0};
-    int dy[] = {0, 0, 1, -1};
-    for (int k = 0; k < 4; k++)
-    {
-        int nx = i + dx[k];
-        int ny = j + dy[k];
-        if (nx >= 0 and ny >= 0 and nx < r and ny < c and grid[nx][ny] == 1 and !visited[nx][ny])
-        {
-            visited[nx][ny] = true;
-            length += dfs(grid, nx, ny, visited, r, c);
-        }
+int dfs(vector<vector<int>> grid, int x, int y, int r, int c, vector<vector<bool>>& visited ){
+
+    visited[x][y] = true;
+    int dx[] = {-1 , 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
+
+    int lengthSoFar = 1;
+    for( int i = 0; i<4; i++){
+        int  nx = x + dx[i], ny = y + dy[i];
+
+        if(grid[nx][ny] == 1 and !visited[nx][ny] and nx<r and ny<c and nx>=0 and ny>= 0)
+            lengthSoFar += dfs(grid, nx, ny,r, c, visited);
     }
-    return length;
+    return lengthSoFar;
 }
-int largest_island(vector<vector<int>> grid)
-{
-    int answer = -1, r = grid.size(), c = grid[0].size();
-    vector<vector<bool>> visited(r, vector<bool>(c, false));
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (!visited[i][j] and grid[i][j] == 1)
-            {
-                visited[i][j] = true;
-                answer = max(answer, dfs(grid, i, j, visited, r, c));
+
+int largestIsland(vector<vector<int>> grid){
+
+
+    int answer = -1;
+    int rows = grid.size();
+    int columns = grid[0].size();
+    vector<vector<bool>> visited (rows, vector<bool>(columns, false));
+    for(int i = 0; i<rows; i++){
+        for(int j = 0; j<columns; j++){
+            if(!visited[i][j] and grid[i][j] == 1){
+                answer = max(answer, dfs(grid, i, j, rows, columns, visited));
             }
         }
     }
-    return ((answer == -1) ? 0 : answer);
+    return (answer == -1)? 0 : answer;
 }

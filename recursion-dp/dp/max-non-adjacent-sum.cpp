3137c0634f1a6f764a7adb77633e49140a80d4e4
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-04-17 04:39:23 +0530

    dp revn ... max-non-adjacent-sum

--- recursion-dp/dp/max-non-adjacent-sum.cpp
+++ recursion-dp/dp/max-non-adjacent-sum.cpp
@@ -1,24 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int max_non_adjacent_sum(vector<int> input)
-{
-    vector<int> dp(input.size(), 0);
-    dp[0] = input[0];
-    dp[1] = max(input[0], input[1]);
-    for (int i = 2; i < input.size(); i++)
-    {
-        dp[i] = max(dp[i - 1], dp[i - 2] + input[i]);
+// int max_non_adjacent_sum(vector<int> input)
+// {
+//     vector<int> dp(input.size(), 0);
+//     dp[0] = input[0];
+//     dp[1] = max(input[0], input[1]);
+//     for (int i = 2; i < input.size(); i++)
+//     {
+//         dp[i] = max(dp[i - 1], dp[i - 2] + input[i]);
+//     }
+//     int max = 0;
+//     for (auto a : dp)
+//     {
+//         if (a > max)
+//         {
+//             max = a;
+//         }
+//     }
+//     return max;
+// }
+
+void maxNonAdjacentSum(int ind, vector<int> values, int n){
+    vector<int>dp (n, 0);
+    dp[0] = values[0];
+    dp[1] = max(dp[0], values[1]);
+    for(int i = 2; i<n; i++){
+        
+        dp[i] = max(dp[i-1], dp[i-2] + values[i]);
+        
     }
-    int max = 0;
-    for (auto a : dp)
-    {
-        if (a > max)
-        {
-            max = a;
-        }
-    }
-    return max;
+    // for(auto a : dp){
+    //     cout<<a<<" ";
+    // }
+    cout<< endl<< dp[n-1];    
 }
 
 int main()

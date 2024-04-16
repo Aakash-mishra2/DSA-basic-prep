d4b4e4b085070b88d4aa647072e042dc2a47480d
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-04-16 07:31:46 +0530

    dp.. revn minimum-jumps.cpp

--- recursion-dp/dp/minimum-jumps.cpp
+++ recursion-dp/dp/minimum-jumps.cpp
@@ -2,22 +2,16 @@
 using namespace std;
 //https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
 //https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/
-int count(int arr[], int n, vector<int>& dp, int ind){
-        if(ind == n-1){ return 0; }
-        if( ind >= n){ return INT_MAX;   }
-        if( dp[ind] != 0){ return dp[ind]; }
-        
-        int maxJumps = arr[ind];
-        int steps = INT_MAX;
-        for(int i = 1; i<= maxJumps; i++){
-            int next_cell = i + ind;
-            int subProb = count(arr, n, dp, next_cell);
-            if(subProb != INT_MAX){
-                steps = min(steps, 1 + subProb);
-            }
-        }
-        return  dp[ind] = steps;
+int maxNonAdjacentSum(vector<int> steps){
+    int n = steps.size();
+    vector<int> dp(n, 0);
+    dp[0] = steps[0];
+    dp[1] = max(dp[0], steps[1]);
+    for(int i = 2; i < n; i++){
+        dp[i] = max(dp[i-2] + steps[i], dp[i-1]);
     }
+    return dp[n-1];
+}
 
 // recursive min array jumps
 int minArrayJumps(vector<int> steps){

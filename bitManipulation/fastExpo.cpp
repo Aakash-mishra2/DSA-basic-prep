9b15851b6f1577ef819756bb0ed1fb34df206317
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-07-09 16:34:18 +0530

    Update and rename evenOdd.cpp to fastExpo.cpp

--- bitManipulation/fastExpo.cpp
+++ bitManipulation/fastExpo.cpp
@@ -12,7 +12,25 @@ void file_io()
     freopen("../arrays-lvl1/revn-1/output.txt", "w", stdout);
 #endif
 }
+int pow(int a, int b){
+    int res = 1;
+    while( b ){
+        if( b&1) res = res * a;
+        a = a*a;
+        b = b>>1;
+}
+    return res;
+}
+int fastMultiply(int a, int b){
+     ll res = 0;
 
+    while(b){
+        if( b & 1) res = res + a;
+        a += a;
+        b = b>>1;
+    }
+    return res;
+}
 int main(){
     file_io();
     int t; cin>>t;

d61f4b4d44f2b12fbe0099c5bd51c882369191e2
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-12-19 16:21:29 +0530

    Update shortestPathGrid.cpp

--- graphs/shortestPathGrid.cpp
+++ graphs/shortestPathGrid.cpp
@@ -1,63 +1,76 @@
 #include <iostream>
-#include <vector>
-#include <set>
-#include <climits>
+#include <bits/stdc++.h>
+
 using namespace std;
-class Node
-{
-public:
+
+class Node {
+    public: 
     int x;
     int y;
     int dist;
-    Node(int x, int y, int dist) : x(x), y(y), dist(dist) {}
-};
-class Compare
-{
-public:
-    bool operator()(const Node &a, const Node &b)
-    {
-        return a.dist <= b.dist;
+    Node(int x, int y, int dist){
+        this->x = x;
+        this->y = y;
+        this->dist = dist;
+    }
+    
+    bool operator<(const Node& other) const {
+        return dist > other.dist;  // We use ">" so the set behaves like a min-heap
     }
 };
 
-int shortest_path(vector<vector<int>> grid)
-{
-    int r = grid.size();
-    int c = grid[0].size();
+// class Compare {
+//     public:
+//         bool operator() (const Node &a, const Node &b){
+//             return a.dist <= b.dist;
+//         }
+// };
+
+int shortest_path(vector<vector<int>> grid){
+    int m = grid.size();
+    int n = grid[0].size();
+    
     int i = 0;
     int j = 0;
-
-    vector<vector<int>> dist(r + 1, vector<int>(c + 1, INT_MAX));
+    
+    vector<vector<int>> dist(m+1, vector<int>(n+1, INT_MAX));
     dist[i][j] = grid[0][0];
-    // set three things i, j, dist sorted based on distance
-    set<Node, Compare> s;
+    set<Node > s;
     s.insert(Node(0, 0, dist[0][0]));
+    
     int dx[] = {0, 0, 1, -1};
     int dy[] = {1, -1, 0, 0};
-    while (!s.empty())
-    {
+    
+    while(!s.empty()){
         auto it = s.begin();
         int cx = it->x;
         int cy = it->y;
-        int cdist = it->dist;
+        int cd = it->dist;
         s.erase(it);
-        for (int k = 0; k < 4; k++)
-        {
+        
+        //update nbrs
+        for(int k = 0; k<4; k++){
             int nx = cx + dx[k];
             int ny = cy + dy[k];
-            if (nx >= 0 and nx < r and ny >= 0 and ny < c and cdist + grid[nx][ny] < dist[nx][ny])
-            {
-
+            if( nx >= 0 and nx<m and ny>=0 and ny<n and cd + grid[nx][ny] < dist[nx][ny]){
                 Node temp(nx, ny, dist[nx][ny]);
-                if (s.find(temp) != s.end())
-                {
-                    s.erase(s.find(temp));
-                }
-
-                dist[nx][ny] = grid[nx][ny] + cdist;
-                s.insert(Node(nx, ny, dist[nx][ny]));
+                if(s.find(temp) != s.end()) s.erase(s.find(temp));
+            
+                //insert the new node in the set
+                int nd = grid[nx][ny] + cd;
+                dist[nx][ny] = nd;
+                s.insert(Node(nx, ny, nd));
             }
         }
     }
-    return dist[r - 1][c - 1];
+    return dist[m-1][n-1];
+}
+
+int main()
+{
+    vector<vector<int>> grid = {{31, 100, 65, 12, 18}, {10, 13, 47, 157, 6}, {100, 113, 174, 11, 33}, {88, 124, 41, 20, 140}};
+    cout<<shortest_path(grid);
+    cout<<endl;
+    
+    return 0;
 }

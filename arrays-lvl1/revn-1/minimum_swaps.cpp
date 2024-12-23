1cfdbb2439a17636652b272959de4b627e514e0f
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-12-24 05:10:02 +0530

    Update minimum_swaps.cpp

--- arrays-lvl1/revn-1/minimum_swaps.cpp
+++ arrays-lvl1/revn-1/minimum_swaps.cpp
@@ -2,8 +2,36 @@
 using namespace std;
 
 // unique - substrings
-int minimum_swaps(vector<int> input, int i = 0)
-{
+int minSwaps(vector<int> arr){
+    //know actual positions store current indices
+    int n = arr.size();
+    pair<int, int> ap[n];
+    for(int i = 0; i<n; i++){
+        ap[i].first = arr[i];
+        ap[i].second = i;
+    }
+    //sorting
+    sort(ap, ap+n);
+    vector<bool> visited(n, false);
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        //if element is visited or element is in right position
+        int old_position = ap[i].second;
+        if(visited[i]== true or old_position == i){
+            continue;
+        }
+
+        //visiting the element (index) for first time;
+        int node = i, cycle = 0;
+        while(!visited[node]){
+            visited[node] = true;
+            int next_node = ap[node].second;
+            node = next_node;
+            cycle += 1;
+        }
+        ans += (cycle - 1);
+    }
+    return ans;
 }
 
 int main()

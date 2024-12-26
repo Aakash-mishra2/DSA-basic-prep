8bddcddff4b856fb3b38574f3b17a266ddf98418
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-12-26 14:26:41 +0530

    Update inversionCount.cpp

--- searching&Sorting/inversionCount.cpp
+++ searching&Sorting/inversionCount.cpp
@@ -1,76 +1,57 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-void file_I_O()
-{
-    ios_base ::sync_with_stdio;
-    cout.tie(0);
-    cin.tie(0);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
-
-int merge(vector<int> &vec, int s, int e)
-{
-    int count = 0;
-    int i = s;
-    int mid = (s + e) / 2;
-    int j = mid + 1;
-    vector<int> temp;
-    while (i <= mid and j <= e)
-    {
-        if (vec[i] < vec[j])
-        {
-            temp.push_back(vec[i]);
-            i++;
+#include <bits/stdc++.h>
+    using namespace std;
+    
+    int merge (vector<int> vec, int s, int e){
+        int count = 0;
+        int i = s;
+        int mid = (s + e)/2;
+        int j = mid + 1;
+        vector<int> temp;
+        
+        while(i <= mid && j<= e){
+            if(vec[i] < vec[j]){
+                temp.push_back(vec[i]);
+                i++;
+            }
+            
+            else {
+                count += (mid - i + 1);
+                temp.push_back(vec[j]);
+                j++;
+            }
         }
-        else
-        {
-            count += mid - i + 1;
-            temp.push_back(vec[j]);
-            j++;
+        
+        while(i<=mid){
+            temp.push_back(vec[i++]);
         }
+        while(j <= e){
+            temp.push_back(vec[j++]);
+        }
+        
+        //copy to original array
+        // int k = 0;
+        // for(int idx = s; idx <= e; idx++){
+        //     vec[idx] = temp[k++];
+        // }
+        return count;
     }
-    while (i <= mid)
-    {
-        temp.push_back(vec[i++]);
+    
+    int invCount(vector<int>& arr, int s, int e){
+        if( s>=e) return 0;
+        
+        int mid = (s + e)/2;
+        int c1 = invCount(arr, s, mid);
+        int c2 = invCount(arr, mid+1, e);
+        int c3 = merge(arr, s, e);
+        
+        return c1 + c2 + c3;
     }
-    while (j <= mid)
+    
+    int main()
     {
-        temp.push_back(vec[j++]);
-    }
-    int k = 0;
-    for (int idx = s; idx <= e; idx++)
-    {
-        vec[idx] = temp[k++];
-    }
-    return count;
-}
-
-int invCount(vector<int> &vec, int s, int e)
-{
-    if (s >= e)
-    {
+       vector<int> arr = {0, 5, 2, 3, 1};
+       int n = arr.size();
+       cout<<invCount(arr, 0, n-1);
+    
         return 0;
     }
-    int mid = (s + e) / 2;
-    invCount(vec, s, mid - 1);
-    invCount(vec, mid, e);
-    return merge(vec, s, e);
-}
-// 10 5 2 0 7 6 4 -- 5
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> arr(n, 0);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    cout << invCount(arr, 0, n - 1);
-    return 0;
-}

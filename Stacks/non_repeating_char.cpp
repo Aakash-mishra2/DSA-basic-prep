53b57755ad94471468bb0fae5c1238929d420808
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2024-12-30 14:14:31 +0530

    Update non_repeating_char.cpp

--- Stacks/non_repeating_char.cpp
+++ Stacks/non_repeating_char.cpp
@@ -24,4 +24,41 @@ int main(){
         else cout<<q.front()<<" ";
     }
     return 0;
+
+    /*
+     file_i_o();
+  queue<char> q;
+  int freq[27] = {0};
+
+  char ch;
+  cin >> ch;
+
+  // prints first non repeating letter so far in string
+  while (ch != '.')
+  {
+    // letter
+    q.push(ch);
+    freq[ch - 'a']++;
+
+    // query
+    while (!q.empty())
+    {
+      int idx = q.front() - 'a';
+
+      if (freq[idx] > 1)
+        q.pop();
+
+      else
+      {
+        cout << q.front() << endl;
+        break;
+      }
+    }
+    if (q.empty())
+      cout << " -1 " << endl;
+    cin >> ch;
+  }
+
+  return 0;
+    */
 }

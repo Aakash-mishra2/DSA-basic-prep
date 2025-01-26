13c60bc795342f50a02e2117f69ef532359a6f07
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2025-01-27 03:08:47 +0530

    Update miscellaneous.cpp

--- strings/miscellaneous.cpp
+++ strings/miscellaneous.cpp
@@ -42,4 +42,48 @@ int main()
     return 0;
 }
 
+//router & houses
+
+ vector<int> houses = {2, 4, 6};
+    vector<int> routers = {4, 5};
+    //find closest and farthest routers;
+    
+    int result = INT_MIN;
+    for(auto house: houses){
+        int it = lower_bound(routers, house);
+        int leftDistance = (it != 0) ? house - routers[it-1] : INT_MAX;
+        int rightDistance = (it != routers.size()-1) ? routers[it] - house : INT_MAX;
+        
+        int minDistance = min(leftDistance, rightDistance);
+        
+        result = max(minDistance, result);
+    }
+    
+    cout<<result<<endl;
+    
+
+// envelope stacking 
+class Solution {
+    static bool cmp(vector<int>& a, vector<int>& b){
+        if(a[0]==b[0]) return a[1] > b[1];
+        return a[0] < b[0];
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& env) {
+        int n = env.size();
+        
+        std::sort(env.begin(), env.end(), cmp);
+        vector<int> dp;
+        for(const auto& envelope : env){
+            int height = envelope[1];
+            auto it = lower_bound(dp.begin(), dp.end(), height);
+            if(it == dp.end()) dp.push_back(height);
+
+            else *it = height;
+        }
+
+        return dp.size();
+    }
+};
+
 

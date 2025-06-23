88121a3db63d55b05dfa2d8056786868fe4bb6c3
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2025-06-23 11:52:48 +0530

    Create accenture.cpp

---
+++ arrays-lvl1/accenture.cpp
@@ -0,0 +1,17 @@
+
+
+find output
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+    int x = 1, y = 1;
+    for(;y; cout<<x<<y<<" "; ){
+        y=x++ <= 5;
+    }
+    return 0;
+}
+
+output; 21 31 41 51 61 70
